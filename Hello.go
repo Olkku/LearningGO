2a67e316e7b103cbddc4dc7c9a3c5edf2e285ee0
“Olkku” <jari.o.soini@gmail.com>
2016-08-30 01:08:22 +0800

    Changed to Println and made a function call as a variable

--- Hello.go
+++ Hello.go
@@ -1,20 +1,19 @@
 package main
 
-import "fmt"
+
+import (
+    "fmt"
+)
 
 func main() {
-    fmt.Printf("   go install github.com/Olkku/hello     <-- compiles the file\n")
-    fmt.Printf("   git push -u origin master             <-- Commits to Github\n\n")
+    p := fmt.Println
 
-    fmt.Printf("   Currently in use:\n")
-    fmt.Printf("   nano Hello.go                         <-- open file to text editor\n")
-    fmt.Printf("   go run hello.go                      <-- Compiles and run the program\n")
-    fmt.Printf("   git commit -a -m \"Comment here\"     <-- Commits to Github\n")
-    fmt.Printf("   git push                              <-- Pushes to Github\n")
+    p("\n   go install github.com/Olkku/hello      <-- compiles the file")
+    p("   git push -u origin master              <-- Commits to Github\n")
 
+    p("   Currently in use:")
+    p("    nano Hello.go                         <-- open file to text editor")
+    p("    go run hello.go                       <-- Compiles and run the program")
+    p("    git commit -a -m \"Comment here\"       <-- Commits to Github")
+    p("    git push                              <-- Pushes to Github\n")
 }
-
-
-//open -a TextEdit Hello.go 
-//go install github.com/Olkku/hello
-//git push -u origin master
