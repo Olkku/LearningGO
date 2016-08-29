c29a570dab662bb924488ee6ef6c8d8ef124ee67
“Olkku” <jari.o.soini@gmail.com>
2016-08-29 23:27:38 +0800

    Added basic instructions how to use Git and Go compiler

--- Hello.go
+++ Hello.go
@@ -6,6 +6,7 @@ func main() {
     fmt.Printf("   nano Hello.go                     <-- open file to text editor\n")
     fmt.Printf("   go install github.com/Olkku/hello <-- compiles the file\n")
     fmt.Printf("   git push -u origin master         <-- Commits to Github\n")
+    fmt.Printf("   git push                          <-- Commits to Github\n")
 }
 
 
